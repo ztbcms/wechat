5049eed48ce71b7735eada99c63f7779d8163858
Jayin <tonjayin@gmail.com>
2023-04-13 15:15:51 +0800

    改进应用信息编辑页

--- view/application/createApplication.php
+++ view/application/createApplication.php
@@ -7,51 +7,51 @@
                     <el-breadcrumb-item>{{form.id>0?'编辑应用':'添加应用'}}</el-breadcrumb-item>
                 </el-breadcrumb>
             </div>
-            <el-form style="width: 800px" :model="form" label-width="130px">
-                <el-form-item label="名称">
+            <el-form style="width: 800px" :model="form" label-width="150px">
+                <el-form-item label="名称" required>
                     <el-input v-model="form.application_name"></el-input>
                 </el-form-item>
-                <el-form-item label="类型">
+                <el-form-item label="类型" required>
                     <el-radio-group v-model="form.account_type">
                         <el-radio label="office">公众号</el-radio>
                         <el-radio label="mini">小程序</el-radio>
                     </el-radio-group>
                 </el-form-item>
-                <el-form-item label="应用别名">
+                <el-form-item label="应用别名" required>
                     <el-input v-model="form.alias"></el-input>
                     <div class="el-tip">调用时方法时使用，避免更换了app_id需要修改代码</div>
                 </el-form-item>
-                <el-form-item label="app_id">
+                <el-form-item label="app_id" required>
                     <el-input v-model="form.app_id"></el-input>
                 </el-form-item>
-                <el-form-item label="secret">
+                <el-form-item label="secret" required>
                     <el-input v-model="form.secret"></el-input>
                 </el-form-item>
-                <el-form-item label="微信支付mch_id">
-                    <el-input v-model="form.mch_id"></el-input>
-                </el-form-item>
-                <el-form-item label="微信支付key">
-                    <el-input v-model="form.mch_key"></el-input>
-                </el-form-item>
-                <el-form-item v-if="form.account_type == 'office'" label="token">
+                <el-form-item v-if="form.account_type == 'office'" label="令牌(Token)">
                     <el-input v-model="form.token"></el-input>
                     <div class="el-tip">接受服务推送消息需要配置令牌token(从微信公众号『开发-基本配置』-『服务器配置』中获取)</div>
                 </el-form-item>
-                <el-form-item v-if="form.account_type == 'office'" label="aes_key">
+                <el-form-item v-if="form.account_type == 'office'" label="消息加解密密钥(EncodingAESKey)">
                     <el-input v-model="form.aes_key"></el-input>
                     <div class="el-tip">消息加解密密钥(从微信公众号『开发-基本配置』-『服务器配置』中获取)</div>
                 </el-form-item>
-                <el-form-item label="支付cert_path">
+                <el-form-item label="微信支付商户号">
+                    <el-input v-model="form.mch_id" placeholder="请输入微信支付商户号（mch_id）"></el-input>
+                </el-form-item>
+                <el-form-item label="微信支付APIv2密钥">
+                    <el-input v-model="form.mch_key" placeholder="请输入API v2 密钥"></el-input>
+                </el-form-item>
+                <el-form-item label="微信支付API证书公钥">
                     <el-input type="textarea"
                               v-model="form.cert_path"
                               :autosize="{ minRows: 2, maxRows: 6 }"
-                              placeholder="请输入微信支付的 apiclient_cert.pem文件内容"></el-input>
+                              placeholder="请输入证书公钥（Certificate），登录商户平台下载 API 证书"></el-input>
                 </el-form-item>
-                <el-form-item label="支付key_path">
+                <el-form-item label="微信支付API证书私钥">
                     <el-input type="textarea"
                               v-model="form.key_path"
                               :autosize="{ minRows: 2, maxRows: 6 }"
-                              placeholder="请输入微信支付的 apiclient_key.pem文件内容"></el-input>
+                              placeholder="请输入证书私钥(Private Key)，登录商户平台下载 API 证书"></el-input>
                 </el-form-item>
                 <el-form-item label="" style="margin-top: 10px;padding-top: 10px;">
                     <el-button type="primary" @click="submitEvent">确定</el-button>
