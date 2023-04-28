2b10011ab609167bc5d8b5a6a2b247de675feefa
Jayin <tonjayin@gmail.com>
2023-04-28 18:46:46 +0800

    Update

--- view/login/office_scan_login/index.php
+++ view/login/office_scan_login/index.php
@@ -37,14 +37,38 @@
 <div id="app" v-cloak>
     <div class="page">
         <div class="qrcode_area">
-            <template v-if="qrcode">
+            <template v-if="status == 1">
+                <div class="loading_continer">
+                    <div class="loading"></div>
+                </div>
+            </template>
+            <template v-if="status == 2">
+                <svg t="1682653612574" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                     xmlns="http://www.w3.org/2000/svg" p-id="3533" width="100" height="100">
+                    <path d="M512.5 128.2c51.9 0 102.2 10.1 149.5 30.2 45.7 19.3 86.8 47 122.1 82.3s63 76.4 82.3 122.1c20 47.3 30.2 97.6 30.2 149.5s-10.1 102.2-30.2 149.5c-19.3 45.7-47 86.8-82.3 122.1s-76.4 63-122.1 82.3c-47.3 20-97.6 30.2-149.5 30.2S410.3 886.1 363 866.1c-45.7-19.3-86.8-47-122.1-82.3s-63-76.4-82.3-122.1c-20-47.3-30.2-97.6-30.2-149.5s10.1-102.2 30.2-149.5c19.3-45.7 47-86.8 82.3-122.1s76.4-63 122.1-82.3c47.3-19.9 97.6-30.1 149.5-30.1m0-64c-247.4 0-448 200.6-448 448s200.6 448 448 448 448-200.6 448-448-200.6-448-448-448z"
+                          fill="#fa5151" p-id="3534"></path>
+                    <path d="M512.5 737.7m-50 0a50 50 0 1 0 100 0 50 50 0 1 0-100 0Z" fill="#fa5151" p-id="3535"></path>
+                    <path d="M576.5 300.8c0-35.4-28.7-64.1-64-64.1s-64 28.7-64 64v0.1l24 301.6c0.5 21.6 18.2 39 40 39s39.4-17.4 40-39l24-301.6z"
+                          fill="#fa5151" p-id="3536"></path>
+                </svg>
+                <p class="msg">获取微信二维码失败</p>
+                <button class="weui-btn weui-btn_primary weui-btn_mini" @click="getQrcode">重新获取</button>
+            </template>
+            <template v-if="status == 3">
                 <img :src="qrcode" style="width: 200px">
                 <p class="msg">使用微信扫一扫登录</p>
             </template>
-            <template v-else>
-                <div class="loading_continer">
-                    <div class="loading" style="width: 100px; height: 100px"></div>
-                </div>
+            <template v-if="status == 4">
+                <svg t="1682653612574" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                     xmlns="http://www.w3.org/2000/svg" p-id="3533" width="100" height="100">
+                    <path d="M512.5 128.2c51.9 0 102.2 10.1 149.5 30.2 45.7 19.3 86.8 47 122.1 82.3s63 76.4 82.3 122.1c20 47.3 30.2 97.6 30.2 149.5s-10.1 102.2-30.2 149.5c-19.3 45.7-47 86.8-82.3 122.1s-76.4 63-122.1 82.3c-47.3 20-97.6 30.2-149.5 30.2S410.3 886.1 363 866.1c-45.7-19.3-86.8-47-122.1-82.3s-63-76.4-82.3-122.1c-20-47.3-30.2-97.6-30.2-149.5s10.1-102.2 30.2-149.5c19.3-45.7 47-86.8 82.3-122.1s76.4-63 122.1-82.3c47.3-19.9 97.6-30.1 149.5-30.1m0-64c-247.4 0-448 200.6-448 448s200.6 448 448 448 448-200.6 448-448-200.6-448-448-448z"
+                          fill="#fa5151" p-id="3534"></path>
+                    <path d="M512.5 737.7m-50 0a50 50 0 1 0 100 0 50 50 0 1 0-100 0Z" fill="#fa5151" p-id="3535"></path>
+                    <path d="M576.5 300.8c0-35.4-28.7-64.1-64-64.1s-64 28.7-64 64v0.1l24 301.6c0.5 21.6 18.2 39 40 39s39.4-17.4 40-39l24-301.6z"
+                          fill="#fa5151" p-id="3536"></path>
+                </svg>
+                <p class="msg">二维码已失效</p>
+                <button class="weui-btn weui-btn_primary weui-btn_mini" @click="getQrcode">重新获取</button>
             </template>
         </div>
     </div>
@@ -98,6 +122,40 @@
         font-size: 17px;
     }
 
+    .weui-btn {
+        position: relative;
+        display: block;
+        width: 184px;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 8px 24px;
+        box-sizing: border-box;
+        font-weight: 700;
+        font-size: 17px;
+        text-align: center;
+        text-decoration: none;
+        color: #fff;
+        line-height: 1.88235294;
+        border-radius: 8px;
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+        -webkit-user-select: none;
+        user-select: none;
+        border: 0;
+    }
+
+    .weui-btn_primary {
+        background-color: #07c160;
+    }
+
+    .weui-btn_mini {
+        display: inline-block;
+        width: auto;
+        line-height: calc((32 - 10) / 16);
+        padding: 5px 12px;
+        font-size: 16px;
+        border-radius: 6px;
+    }
+
 </style>
 
 <script>
@@ -111,13 +169,15 @@
                 intervalId: 0,
                 timeoutId: 0,
                 checkCodeRepeatTime: 2 * 1000,
-                redirect_url: "{$redirect_url}"
+                redirect_url: "{$redirect_url}",
+                status: 0,// 1拉取中 2拉取失败 3二维码生效中 4二维码已失效
             },
             mounted: function () {
                 this.getQrcode()
             },
             methods: {
                 getQrcode: function () {
+                    this.status = 1
                     let that = this
                     if (this.intervalId) {
                         clearInterval(this.intervalId)
@@ -130,8 +190,11 @@
                             that.code = res.data.code
                             that.qrcode = res.data.qrcode
                             that.ttl = res.data.ttl
-                            that.timoutId = setTimeout(that.getQrcode, that.ttl * 1000)
+                            that.timoutId = setTimeout(that.doExipredCode, that.ttl * 1000)
                             that.intervalId = setInterval(that.checkCode, that.checkCodeRepeatTime)
+                            that.status = 3
+                        } else {
+                            that.status = 2
                         }
                     })
                 },
@@ -156,6 +219,17 @@
                             }
                         }
                     })
+                },
+                // 使二维码过期
+                doExipredCode: function(){
+                    this.code = ''
+                    if (this.intervalId) {
+                        clearInterval(this.intervalId)
+                    }
+                    if (this.timeoutId) {
+                        clearTimeout(this.timeoutId)
+                    }
+                    this.status = 4
                 }
             }
         })
