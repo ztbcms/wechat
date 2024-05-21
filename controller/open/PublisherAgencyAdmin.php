409b37f77af8be6b12d12cc90c5f10ce47cae6be
Jayin <tonjayin@gmail.com>
2024-05-21 22:09:42 +0800

    `open.PublisherAgencyAdmin/adUnits:setAdUnitStatus` Fix api param name

--- controller/open/PublisherAgencyAdmin.php
+++ controller/open/PublisherAgencyAdmin.php
@@ -221,8 +221,8 @@ class PublisherAgencyAdmin extends AdminController
             $authorizer_appid = input('post.authorizer_appid');
             $ad_unit_id = input('post.ad_unit_id');
             $name = input('post.name');
-            $status = input('post.status');
-            $status = $status == 1 ? 'AD_UNIT_STATUS_ON' : 'AD_UNIT_STATUS_OFF';
+            $ad_unit_status = input('post.ad_unit_status');
+            $status = $ad_unit_status == 1 ? 'AD_UNIT_STATUS_ON' : 'AD_UNIT_STATUS_OFF';
             $resp = OpenService::getInstnace()->publisherAgency()->agencyUpdateAdunit($authorizer_appid, $ad_unit_id, $name, $status);
             if (!RequestUtils::isRquestSuccessed($resp)) {
                 return self::createReturn(false, $resp, RequestUtils::buildErrorMsg($resp));
