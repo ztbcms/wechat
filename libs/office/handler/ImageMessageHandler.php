be09a510ca5b67b9ec315335c779ae99e95ee4d7
Jayin Ton <tonjayin@gmail.com>
2025-01-19 13:15:36 +0800

    docs: update

--- libs/office/handler/ImageMessageHandler.php
+++ libs/office/handler/ImageMessageHandler.php
@@ -5,6 +5,10 @@
 
 namespace app\wechat\libs\office\handler;
 
+/**
+ * 图片消息
+ * @see https://developers.weixin.qq.com/doc/offiaccount/Message_Management/Receiving_standard_messages.html#图片消息
+ */
 class ImageMessageHandler implements EventHandlerInterface
 {
 
--- libs/office/handler/TextMessageHandler.php
+++ libs/office/handler/TextMessageHandler.php
@@ -5,8 +5,10 @@
 
 namespace app\wechat\libs\office\handler;
 
-use EasyWeChat\Kernel\Messages\Text;
-
+/**
+ * 文本消息
+ * @see https://developers.weixin.qq.com/doc/offiaccount/Message_Management/Receiving_standard_messages.html#文本消息
+ */
 class TextMessageHandler implements EventHandlerInterface
 {
 
