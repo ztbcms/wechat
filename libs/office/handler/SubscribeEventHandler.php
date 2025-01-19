38829d0fdefcdec7512bfa3f4cb67043359e6550
Jayin Ton <tonjayin@gmail.com>
2025-01-19 12:46:15 +0800

    clean: 移除废弃代码

--- libs/office/handler/SubscribeEventHandler.php
+++ libs/office/handler/SubscribeEventHandler.php
@@ -5,10 +5,7 @@
 
 namespace app\wechat\libs\office\handler;
 
-use app\common\service\jwt\JwtService;
 use app\wechat\service\login\ScanLoginService;
-use EasyWeChat\Kernel\Messages\Text;
-use think\facade\Cache;
 
 /**
  * 关注事件
@@ -23,32 +20,6 @@ class SubscribeEventHandler implements EventHandlerInterface
             return ScanLoginService::handleOfficeScanLogin($appid, $msg_payload);
         }
 
-        return null;
-    }
-
-    /**
-     * 扫码登录业务逻辑
-     * @param $appid
-     * @param array $msg_payload
-     * @return Text
-     */
-    function handleOfficeScanLogin($appid, array $msg_payload)
-    {
-        $jwtService = new JwtService();
-        $info = [
-            'app_id' => $appid,
-            'open_id' => $msg_payload['FromUserName'],
-            'login_code' => str_replace('qrscene_', '', $msg_payload['EventKey']),
-            'exp' => time() + 30,
-        ];
-        $token = Cache::get(ScanLoginService::getLoginCodeCacheKey($info['login_code']));
-        if ($token === null) {
-            $token = $jwtService->createToken($info);
-            $url = api_url('wechat/login.OfficeScanLogin/confirmLogin', ['code' => $token]);
-            // 登录码标识为空，即用户已扫码
-            Cache::set(ScanLoginService::getLoginCodeCacheKey($info['login_code']), '', 5 * 60);
-            return new Text("<a href='{$url}'>点击此处确认登录</a>");
-        }
         return null;
     }
 }
